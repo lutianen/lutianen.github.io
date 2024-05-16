1337ba9eade7f061668e2a2fc7fbc9f2a77da942
lutianen <tianen.xd@gmail.com>
2024-05-17 01:04:21 +0800

    rebuilding site Fri May 17 01:04:21 AM CST 2024

--- posts/GMP-of-Go/main.go
+++ posts/GMP-of-Go/main.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
 func main() {
+	wg := sync.WaitGroup{}
 	fmt.Println("Hello, World!")
+
+	wg.Add(1)
+	go f(&wg)
+
+	wg.Wait()
+}
+
+func f(wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	fmt.Println("goroutine")
 }
